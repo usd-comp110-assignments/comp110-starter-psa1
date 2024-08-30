bd5c80521c892f5a5329e1a10033ca72d66a5610
Sophia Krause-Levy <skrauselevy@sandiego.edu>
2024-08-30 10:03:26 -0700

    starter code

---
+++ name_drawer.py
@@ -0,0 +1,14 @@
+"""
+Module: name_drawer
+
+A program to draw a name using one (or more) turtles.
+
+Authors:
+1) Name - USD Email Address
+2) Name - USD Email Address
+"""
+import turtle
+
+# Add your code here, and delete this comment once you start.
+
+print("COMP110 PSA1. Delete this line after you start editing.")
